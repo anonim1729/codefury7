82d4b1f640f8d2aef0ff2e7b4ed81e9ede07598a
Sachin <sachinkashi21@gmail.com>
2024-08-10 21:44:42 +0530

    Home page and Disaster

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './Hero';
 import Card from '../components/Card'; 
+import CreaterCard from '../components/CreaterCard';
 
 const Home = () => {
   return (
@@ -16,6 +17,16 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className="container mx-auto px-4 py-10">
+  <h2 className="text-3xl font-bold text-blue-600 text-center mb-8">About The Creators</h2>
+  <div className="flex flex-wrap justify-center space-x-4">
+    <CreaterCard name="Narendra" image="" role="MERN stack Developer" url=""/>
+    <CreaterCard name="Sachin" image="" role="MERN stack Developer" url=""/>
+    <CreaterCard name="Sajid" image="" role="Database & Backend Developer" url=""/>
+  </div>
+</div>
+
     </>
   );
 };
